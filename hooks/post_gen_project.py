df1c2ef9eb781ab11ed811f3a5e3e632d23c2fa4
Artem Osadchyi <249983576+r73m@users.noreply.github.com>
2026-01-10 16:52:28 +0100

    Improve commit message (#4)
    - Set commit title to a more conventional `Initial commit`
    - Include a command that could be used to generate a project

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,20 +5,42 @@ from typing import Any
 
 def run(*commands):
     for command in commands:
+        if isinstance(command, str):
+            command = command.split()
         subprocess.run(command, check=True)
+
+
+def to_shell(value):
+    if isinstance(value, bool):
+        return "y" if value else "n"
+    value = str(value)
+    if " " in value:
+        return f"'{value}'"
+    return value
 
 
 params: OrderedDict[str, Any] = {{ cookiecutter }}  # type: ignore  # noqa: F821  # fmt: skip
 
+suffix = " \\\n"
+commit_message = f"""\
+Initial commit
+
+Generated with an equivalent of the following command:
+cookiecutter \\
+    --no-input --checkout {params["_template_version"]} \\
+    gh:r73m/cookiecutter-python-app \\
+{
+    suffix.join(
+        f"    {key}={to_shell(value)}"
+        for key, value in params.items()
+        if not key.startswith("_")
+    )
+}
+"""
+
 run(
-    ["uv", "lock"],
-    ["git", "init"],
-    ["git", "add", "."],
-    [
-        "git", "commit",
-        "--message", params["description"],
-        "--message", "Bootstrapped from https://github.com/r73m/cookiecutter-python-app",
-    ],
+    "uv lock", "git init", "git add .",
+    ["git", "commit", "--message", commit_message],
 )  # fmt: skip
 
 if params["github"] in [True, "y"]:
